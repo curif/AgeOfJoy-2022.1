a0b72d28c3f475aa2d06a0205acb6577b8f664ea
curif <curif50@gmail.com>
2023-04-18 08:46:59 -0300

    user can't move after insert coin #122

--- Assets/curif/LibRetroWrapper/ChangeControls.cs
+++ Assets/curif/LibRetroWrapper/ChangeControls.cs
@@ -2,63 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class ChangeControls : MonoBehaviour
 {
-  public GameObject leftHandXRControl, rightHandXRControl;
-  public GameObject leftHandPrefab;
-  public GameObject rightHandPrefab;
-  public GameObject leftJoystickPrefab;
-  public GameObject rightJoystickPrefab;
-  
-  ActionBasedController controllerLeftHand;
-  ActionBasedController controllerRightHand;
+    public GameObject leftHandXRControl, rightHandXRControl;
+    public GameObject leftHandPrefab;
+    public GameObject rightHandPrefab;
+    public GameObject leftJoystickPrefab;
+    public GameObject rightJoystickPrefab;
 
-  GameObject leftHandModel;
-  GameObject rightHandModel;
-  GameObject leftJoystickModel;
-  GameObject rightJoystickModel;
+    DynamicMoveProvider dynamicMoveProvider;
+    float moveSpeed;
+    ActionBasedContinuousTurnProvider actionBasedContinuousTurnProvider;
+    float turnSpeed;
+    ActionBasedController controllerLeftHand;
+    ActionBasedController controllerRightHand;
+    GameObject leftHandModel;
+    GameObject rightHandModel;
+    GameObject leftJoystickModel;
+    GameObject rightJoystickModel;
 
-  void Start()
-  {
-    controllerLeftHand = leftHandXRControl.GetComponent<ActionBasedController>();
-    controllerRightHand = rightHandXRControl.GetComponent<ActionBasedController>();
-    
-    leftJoystickModel = GameObject.Instantiate(leftJoystickPrefab, controllerLeftHand.modelParent);
-    rightJoystickModel = GameObject.Instantiate(rightJoystickPrefab, controllerRightHand.modelParent);
-    leftHandModel = GameObject.Instantiate(leftHandPrefab, controllerLeftHand.modelParent);
-    rightHandModel = GameObject.Instantiate(rightHandPrefab, controllerRightHand.modelParent);
-
-    controllerLeftHand.model = leftHandModel.transform;
-    controllerRightHand.model = rightHandModel.transform;
-
-    PlayerMode(false);
-  }
-  
-  public void PlayerMode(bool modePlaying)
-  {
-    ConfigManager.WriteConsole($"[ChangeControls.Playermode] modePlaying: {modePlaying}");
-    if (modePlaying) 
+    void Start()
     {
-      leftJoystickModel.SetActive(true);
-      rightJoystickModel.SetActive(true);
-      leftHandModel.SetActive(false);
-      rightHandModel.SetActive(false);
-      controllerLeftHand.model = leftJoystickModel.transform;
-      controllerRightHand.model = rightJoystickModel.transform;
-      controllerLeftHand.enableInputActions = false;
-      controllerRightHand.enableInputActions = false;
+        controllerLeftHand = leftHandXRControl.GetComponent<ActionBasedController>();
+        controllerRightHand = rightHandXRControl.GetComponent<ActionBasedController>();
+
+        actionBasedContinuousTurnProvider = GetComponent<ActionBasedContinuousTurnProvider>();
+        dynamicMoveProvider = GetComponent<DynamicMoveProvider>();
+        turnSpeed = actionBasedContinuousTurnProvider.turnSpeed;
+        moveSpeed = dynamicMoveProvider.moveSpeed;
+
+        leftJoystickModel = GameObject.Instantiate(leftJoystickPrefab, controllerLeftHand.modelParent);
+        rightJoystickModel = GameObject.Instantiate(rightJoystickPrefab, controllerRightHand.modelParent);
+        leftHandModel = GameObject.Instantiate(leftHandPrefab, controllerLeftHand.modelParent);
+        rightHandModel = GameObject.Instantiate(rightHandPrefab, controllerRightHand.modelParent);
+
+        controllerLeftHand.model = leftHandModel.transform;
+        controllerRightHand.model = rightHandModel.transform;
+
+        PlayerMode(false);
     }
-    else
+
+    public void PlayerMode(bool modePlaying)
     {
-      controllerLeftHand.model = leftHandModel.transform;
-      controllerRightHand.model = rightHandModel.transform;
-      leftJoystickModel.SetActive(false);
-      rightJoystickModel.SetActive(false);
-      leftHandModel.gameObject.SetActive(true);
-      rightHandModel.gameObject.SetActive(true);
-      controllerLeftHand.enableInputActions = true;
-      controllerRightHand.enableInputActions = true;
+        ConfigManager.WriteConsole($"[ChangeControls.Playermode] modePlaying: {modePlaying}");
+        if (modePlaying)
+        {
+            leftJoystickModel.SetActive(true);
+            rightJoystickModel.SetActive(true);
+            leftHandModel.SetActive(false);
+            rightHandModel.SetActive(false);
+            controllerLeftHand.model = leftJoystickModel.transform;
+            controllerRightHand.model = rightJoystickModel.transform;
+            actionBasedContinuousTurnProvider.turnSpeed = 0;
+            dynamicMoveProvider.moveSpeed = 0;          
+        }
+        else
+        {
+            controllerLeftHand.model = leftHandModel.transform;
+            controllerRightHand.model = rightHandModel.transform;
+            leftJoystickModel.SetActive(false);
+            rightJoystickModel.SetActive(false);
+            leftHandModel.gameObject.SetActive(true);
+            rightHandModel.gameObject.SetActive(true);
+            actionBasedContinuousTurnProvider.turnSpeed = turnSpeed;
+            dynamicMoveProvider.moveSpeed = moveSpeed;
+        }
     }
-  }
 }
