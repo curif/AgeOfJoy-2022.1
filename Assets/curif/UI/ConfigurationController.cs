2dae6da9efd509aeaf23dd9fc20ec7496d4583e7
--global <curif50@gmail.com>
2023-05-17 08:51:27 -0300

    #122 C64 Menu to BTrees

--- Assets/curif/UI/ConfigurationController.cs
+++ Assets/curif/UI/ConfigurationController.cs
@@ -5,6 +5,8 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
+using CleverCrow.Fluid.BTs.Tasks;
+using CleverCrow.Fluid.BTs.Trees;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -12,21 +14,21 @@ using UnityEditor;
 public class ConfigurationController : MonoBehaviour
 {
     public ScreenGenerator scr;
-
-    private DefaultControlMap map;
     public CoinSlotController CoinSlot;
     public InputActionMap actionMap;
     public ChangeControls changeControls;
 
-  [Tooltip("Set only to change room configuration, if not setted will use the Global")]
-  public GameObject RoomConfigurationGameObject;
-  private RoomConfiguration roomConfiguration;
-  [Tooltip("Set to change the Global (if not setted the Room Config)")]
-  public GameObject GlobalConfigurationGameObject;
-  private GlobalConfiguration globalConfiguration;
+    [Tooltip("Set only to change room configuration, if not setted will use the Global")]
+    public GameObject RoomConfigurationGameObject;
+    [Tooltip("Set to change the Global (if not setted the Room Config)")]
+    public GameObject GlobalConfigurationGameObject;
 
-  [Tooltip("Set room or Global configuration")]
-  public ConfigInformation Configuration;
+    [Tooltip("Set room or Global configuration")]
+    public ConfigInformation Configuration;
+
+    [Header("Tree")]
+    [SerializeField]
+    public BehaviorTree tree;
 
     private GenericMenu mainMenu;
     private enum StatusOptions
@@ -43,6 +45,9 @@ public class ConfigurationController : MonoBehaviour
 
     private string NPCStatus;
     private GenericOptions NPCStatusOptions; 
+    private DefaultControlMap map;
+    private GlobalConfiguration globalConfiguration;
+    private RoomConfiguration roomConfiguration;
 
     // Start is called before the first frame update
     void Start()
@@ -69,9 +74,6 @@ public class ConfigurationController : MonoBehaviour
     }
     private void setMainMenu()
     {
-      string[] options = new string[] {"Sound configuration", "NPC configuration", "Controllers"};
-      string[] helpText = new string[] { "Change sound volume", "To change the NPC behavior", "Map your control to play games" };
-      mainMenu = new(scr, "AGE of Joy - Main configuration", options, helpText);
     }
 
     private bool isRoomConfiguration()
@@ -83,30 +85,8 @@ public class ConfigurationController : MonoBehaviour
       return globalConfiguration != null;
     }
 
-    private void reset()
+    public void NPCScreen()
     {
-      setMainMenu();
-      ControlEnable(false);
-      showInsertCoinMessage();
-      status = StatusOptions.waitingForCoin;
-    }
-
-    public void InsertCoin()
-    {
-      ControlEnable(true);
-      status = StatusOptions.onBoot;
-      scr.Clear();
-      onBootDelayWaiter = new(3); 
-    }
-    public void GoToMainMenu()
-    {
-      status = StatusOptions.onMainMenu;
-      scr.Clear();
-      mainMenu.DrawMenu();
-    }
-    public void GoToNPCConfiguration()
-    {
-      status = StatusOptions.onNPCMenu;
       scr.Clear();
       scr.PrintCentered(9, "-- NPC configuration --");
 
@@ -126,18 +106,21 @@ public class ConfigurationController : MonoBehaviour
       NPCStatusOptions.SetCurrent(actualNPCStatus);
       NPCStatusOptions.Print();
     }
+    public void InsertCoin()
+    {
+      ControlEnable(true);
+      status = StatusOptions.onBoot;
+      scr.Clear();
+    }
 
     IEnumerator run()
     {
       float timeBetweenCycles = 1f/3f;
+      string[] options = new string[] {"Sound configuration", "NPC configuration", "Controllers"};
+      string[] helpText = new string[] { "Change sound volume", "To change the NPC behavior", "Map your control to play games" };
 
-      setMainMenu();
-
-      status = StatusOptions.init;
-      
-      //C64 Boot screen
+      mainMenu = new(scr, "AGE of Joy - Main configuration", options, helpText);
       bootScreen = new(scr);
-      onBootDelayWaiter = new(3); 
 
       //NPC configuration options.
       // take the statuses from the static value options in the information NPC class.
@@ -145,95 +128,136 @@ public class ConfigurationController : MonoBehaviour
 
       yield return new WaitForSeconds(2f);
 
+      status = StatusOptions.init;
+      tree = buildBT();
       while (true)
       {
-        ConfigManager.WriteConsole($"[ConfigurationController.run] status: {status}");
-        if (status == StatusOptions.init)
-        {
-          reset();
-        }
-
-        bool takeCoin = (CoinSlot != null && CoinSlot.takeCoin()) || ControlActive("INSERT");
-        if (takeCoin && status == StatusOptions.waitingForCoin)
-        {
-          ConfigManager.WriteConsole($"[ConfigurationController.run] INSERT COIN status: {status}");
-          InsertCoin();    
-        }
-
-        if (status == StatusOptions.waitingForCoin)
-          yield return new WaitForSeconds(timeBetweenCycles);
-
-        if (status == StatusOptions.onBoot)
-        {
-          if (onBootDelayWaiter.Finished())
-            GoToMainMenu();
-          else
-            bootScreen.PrintNextLine();
-          yield return new WaitForSeconds(timeBetweenCycles);
-        }
-
-        if (ControlActive("EXIT"))
-        {
-          ConfigManager.WriteConsole($"[ConfigurationController.run] EXIT status: {status}");
-          reset();
-          yield return new WaitForSeconds(timeBetweenCycles);
-        }
-
-        if (status == StatusOptions.onMainMenu)
-        {
-          if (ControlActive("JOYPAD_UP") || ControlActive("KEYB-UP"))
-            mainMenu.PreviousOption();
-          else if (ControlActive("JOYPAD_DOWN")|| ControlActive("KEYB-DOWN"))
-            mainMenu.NextOption();
-          else if (ControlActive("JOYPAD_B"))
-            mainMenu.Select();
-
-          if (mainMenu.IsSelected())
-          {
-            ConfigManager.WriteConsole($"[ConfigurationController.Update] option selected: {mainMenu.GetSelectedOption()}");
-            if (mainMenu.GetSelectedOption() == "NPC configuration")
-            {
-              status = StatusOptions.onNPCMenu;
-              GoToNPCConfiguration();
-            }
-            mainMenu.Deselect();
-          }
-        }
-        else if (status == StatusOptions.onNPCMenu)
-        {
-          NPCStatusOptions.Print();
-          if (ControlActive("JOYPAD_LEFT")|| ControlActive("KEYB-LEFT"))
-            NPCStatusOptions.PreviousOption();
-          else if (ControlActive("JOYPAD_RIGHT")|| ControlActive("KEYB-RIGHT"))
-            NPCStatusOptions.NextOption();
-          else if (ControlActive("JOYPAD_B"))
-          {
-            if (isGlobalConfiguration())
-            {
-              if (globalConfiguration.Configuration.npc == null)
-                globalConfiguration.Configuration.npc = new();
-              globalConfiguration.Configuration.npc.status = NPCStatusOptions.GetSelectedOption();
-            }
-            else if (isRoomConfiguration())
-            {
-              if (roomConfiguration.Configuration.npc == null)
-                roomConfiguration.Configuration.npc = new();
-              roomConfiguration.Configuration.npc.status = NPCStatusOptions.GetSelectedOption();
-            }
-            // TODO SAVE
-            GoToMainMenu();
-          }
-        }
-        
+        tree.Tick();
         yield return new WaitForSeconds(timeBetweenCycles);
       }
     }
-
-    private void showInsertCoinMessage()
+    private BehaviorTree buildBT()
     {
-      scr.Clear();
-      scr.PrintCentered(10, "Insert coin to start", true);
+      return new BehaviorTreeBuilder(gameObject)
+        .Selector()
+
+          .Sequence("Init")
+            .Condition("On init", () => status == StatusOptions.init)
+            .Do("Process", () =>
+              {
+                ControlEnable(false);
+                status = StatusOptions.waitingForCoin;
+                scr.Clear();
+                scr.PrintCentered(10, "Insert coin to start", true);
+                return TaskStatus.Success;
+              })
+          .End()
+
+          .Sequence("Insert coin")
+            .Condition("Waiting for coin", () => status == StatusOptions.waitingForCoin)
+            .Condition("Is a coin in the bucket",() => (CoinSlot != null && CoinSlot.takeCoin()) || ControlActive("INSERT"))
+            .Do("coin inserted", () => 
+              {
+                InsertCoin();
+                return TaskStatus.Success;
+              })
+          .End()
+
+          .Sequence("Boot")
+            .Condition("Booting", () => status == StatusOptions.onBoot)
+            .WaitTime(0.25f)
+            .Condition("Finished lines", () => bootScreen.PrintNextLine())
+            .Do("Start main menu", () => 
+              {
+                status = StatusOptions.onMainMenu;
+                return TaskStatus.Success;
+              })
+          .End()
+
+          .Sequence("Main menu")
+            .Condition("On main menu",  () => status == StatusOptions.onMainMenu)
+            .Do("Init", () =>
+              {
+                scr.Clear();
+                mainMenu.DrawMenu();
+                return TaskStatus.Success;
+              })
+            .Do("Process", () =>
+              {
+                if (ControlActive("JOYPAD_UP") || ControlActive("KEYB-UP"))
+                  mainMenu.PreviousOption();
+                else if (ControlActive("JOYPAD_DOWN") || ControlActive("KEYB-DOWN"))
+                  mainMenu.NextOption();
+                else if (ControlActive("JOYPAD_B"))
+                  mainMenu.Select();
+
+                if (!mainMenu.IsSelected())
+                  return TaskStatus.Continue;
+
+                ConfigManager.WriteConsole($"[ConfigurationController] option selected: {mainMenu.GetSelectedOption()}");
+                if (mainMenu.GetSelectedOption() == "NPC configuration")
+                {
+                  status = StatusOptions.onNPCMenu;
+                }
+                mainMenu.Deselect();
+                return TaskStatus.Success;
+              })
+          .End()
+
+          .Sequence("NPC Configuration")
+            .Condition("On NPC Config", () => status == StatusOptions.onNPCMenu)
+            .Do("Init", () =>
+              {
+                NPCScreen();
+                return TaskStatus.Success;
+              })
+            .Do("Process", () =>
+              {
+                NPCStatusOptions.Print();
+                if (ControlActive("JOYPAD_LEFT")|| ControlActive("KEYB-LEFT"))
+                  NPCStatusOptions.PreviousOption();
+                else if (ControlActive("JOYPAD_RIGHT")|| ControlActive("KEYB-RIGHT"))
+                  NPCStatusOptions.NextOption();
+                else if (ControlActive("JOYPAD_B"))
+                {
+                  return TaskStatus.Success;
+                }
+                return TaskStatus.Continue;
+              })
+            .Do("Save", () =>
+              {
+                if (isGlobalConfiguration())
+                {
+                  if (globalConfiguration.Configuration.npc == null)
+                    globalConfiguration.Configuration.npc = new();
+                  globalConfiguration.Configuration.npc.status = NPCStatusOptions.GetSelectedOption();
+                }
+                else if (isRoomConfiguration())
+                {
+                  if (roomConfiguration.Configuration.npc == null)
+                    roomConfiguration.Configuration.npc = new();
+                  roomConfiguration.Configuration.npc.status = NPCStatusOptions.GetSelectedOption();
+                }
+                
+                status = StatusOptions.onMainMenu;
+
+                return TaskStatus.Success;
+              })
+          .End() 
+
+          .Sequence("EXIT")
+            .Condition("Exit button", () => ControlActive("EXIT"))
+            .Do("exit", () =>
+            {
+              ConfigManager.WriteConsole($"[ConfigurationController] EXIT ");
+              status = StatusOptions.init;
+              return TaskStatus.Success;
+            })
+          .End()
+        .End()
+        .Build();
     }
+
     public void ControlEnable(bool enable)
     {
       changeControls.PlayerMode(enable);
