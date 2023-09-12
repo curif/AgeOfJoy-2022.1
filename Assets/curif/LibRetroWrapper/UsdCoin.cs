2f82e65a9e38e8415bfd712dbd594c93a6f5c7ad
curif <curif50@gmail.com>
2023-09-12 10:34:33 -0300

    a better coin management

--- Assets/curif/LibRetroWrapper/UsdCoin.cs
+++ Assets/curif/LibRetroWrapper/UsdCoin.cs
@@ -8,59 +8,67 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class UsdCoin : MonoBehaviour
 {
 
-    [SerializeField]
-    bool isGrabbed = false;
+  [SerializeField]
+  bool isGrabbed = false;
+  public CoinSlotController ctrl;
 
-    XRGrabInteractable grabbable;
-    GameObject CoinSlot;
-    //this object must be attached to a hand
-    Transform originalParent;
-    Vector3 originalLocalPosition;
-    Quaternion originalLocalRotation;
-    
-    // called from the SelectEventArgs in the XRGrabInteractable Interactable events.
-    public void Selected()
+  GameObject CoinSlot;
+  //this object must be attached to a hand
+  Transform originalParent;
+  Vector3 originalLocalPosition;
+  Quaternion originalLocalRotation;
+
+  void Start()
+  {
+    CoinSlot = null;
+    preserveOriginalValues();
+  }
+
+  void preserveOriginalValues()
+  {
+    originalParent = gameObject.transform.parent;
+    originalLocalPosition = gameObject.transform.localPosition;
+    originalLocalRotation = gameObject.transform.localRotation;
+  }
+
+  void restoreOriginalValues()
+  {
+    gameObject.transform.parent = originalParent;
+    gameObject.transform.localPosition = originalLocalPosition;
+    gameObject.transform.localRotation = originalLocalRotation;
+  }
+
+  void OnTriggerEnter(Collider col)
+  {
+    //Debug.Log($" OnTriggerEnter Collision with  {col.gameObject.name}");
+    if (col.gameObject.tag == "CoinSlot" && isGrabbed)
     {
-      isGrabbed = true;
+      CoinSlot = col.gameObject;
     }
-    public void UnSelected()
+  }
+
+  // called from the SelectEventArgs  
+  // in the XRGrabInteractable Interactable events.
+  public void Selected()
+  {
+    isGrabbed = true;
+    // ConfigManager.WriteConsole($"[UsdCoin.Selected]");
+  }
+
+  public void UnSelected()
+  {
+    isGrabbed = false;
+    // ConfigManager.WriteConsole($"[UsdCoin.UnSelected]");
+
+    if (CoinSlot != null)
     {
-      isGrabbed = false;
+      ConfigManager.WriteConsole($"[UsdCoin.UnSelected] Dropped coin on {CoinSlot.name}");
+      ctrl = CoinSlot.GetComponent<CoinSlotController>();
+      ctrl.insertCoin();
+      CoinSlot = null;
     }
-
-    void Start() {
-
-        grabbable = gameObject.GetComponent<XRGrabInteractable>();
-        // Debug.Log($"obtained grabbable: {grabbable}");
-        CoinSlot = null;
-
-        originalParent = gameObject.transform.parent;
-        originalLocalPosition = gameObject.transform.localPosition;
-        originalLocalRotation = gameObject.transform.localRotation;
-    }
-
-    void Update() {
-
-      if (!isGrabbed) {
-        if (CoinSlot != null) {
-            ConfigManager.WriteConsole($"Dropped coin on {CoinSlot.name}");
-            var ctrl = CoinSlot.GetComponent<CoinSlotController>();
-            ctrl.insertCoin();
-            CoinSlot = null;
-        }
-        gameObject.transform.parent = originalParent;
-        gameObject.transform.localPosition = originalLocalPosition;
-        gameObject.transform.localRotation = originalLocalRotation;
-      }
-    }
-
-    void OnTriggerEnter(Collider col) {
-      //Debug.Log($" OnTriggerEnter Collision with  {col.gameObject.name}");
-      if (col.gameObject.tag == "CoinSlot" && isGrabbed)
-      {
-        CoinSlot = col.gameObject;
-      }
-    }
+    restoreOriginalValues();
+  }
 
 }
 
