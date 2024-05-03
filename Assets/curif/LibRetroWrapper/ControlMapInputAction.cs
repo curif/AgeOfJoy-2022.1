44d2fe0844a85fc7f41e3b91b659582b1ccf9969
Fabio Curi <curif50@gmail.com>
2024-05-03 07:52:43 -0300

    Jukebox #344 adding a path to a control map wasn't working.

--- Assets/curif/LibRetroWrapper/ControlMapInputAction.cs
+++ Assets/curif/LibRetroWrapper/ControlMapInputAction.cs
@@ -12,36 +12,41 @@ using System.Text;
 public static class ControlMapInputAction
 {
 
-  public static InputActionMap inputActionMapFromConfiguration(ControlMapConfiguration mapConfig, string name = null)
-  {
-    InputActionMap inputActionMap = new(name);
-    foreach (var map in mapConfig.mapList)
+    public static InputActionMap inputActionMapFromConfiguration(ControlMapConfiguration mapConfig, string name = null)
     {
-      InputActionType t = InputActionType.Button;
-      if (map.behavior == "axis")
-      {
-        t = InputActionType.Value;
-      }
-      InputAction action = inputActionMap.AddAction(map.InputActionMapName(), type: t);
-      foreach (var controlMap in map.controlMaps)
-      {
-        string path = ControlMapPathDictionary.GetInputPath(controlMap.RealControl);
-        if (string.IsNullOrEmpty(path))
+        InputActionMap inputActionMap = new(name);
+        foreach (var map in mapConfig.mapList)
         {
-          ConfigManager.WriteConsoleError($"[inputActionMapFromConfiguration] control {controlMap.RealControl} doesn't have a path. ");
+            InputActionType t = InputActionType.Button;
+            if (map.behavior == "axis")
+            {
+                t = InputActionType.Value;
+            }
+            InputAction action = inputActionMap.AddAction(map.InputActionMapName(), type: t);
+            foreach (var controlMap in map.controlMaps)
+            {
+                string path;
+                if (string.IsNullOrEmpty(controlMap.Path))
+                    path = ControlMapPathDictionary.GetInputPath(controlMap.RealControl);
+                else
+                    path = controlMap.Path;
+
+                if (string.IsNullOrEmpty(path))
+                {
+                    ConfigManager.WriteConsoleError($"[inputActionMapFromConfiguration] control {controlMap.RealControl} doesn't have a path and wasn't specified.");
+                }
+                else
+                {
+                    var bind = new InputBinding
+                    {
+                        path = path,
+                        action = map.mameControl + "_" + map.port.ToString()
+                    };
+                    action.AddBinding(bind);
+                }
+            }
         }
-        else
-        {
-          var bind = new InputBinding
-          {
-            path = path, 
-            action = map.mameControl + "_" + map.port.ToString()
-          };
-          action.AddBinding(bind);
-        }
-      }
+        return inputActionMap;
     }
-    return inputActionMap;
-  }
 
 }
