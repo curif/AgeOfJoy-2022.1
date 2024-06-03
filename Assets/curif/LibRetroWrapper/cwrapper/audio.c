b8494b0e639faa5acef6adcf6cc9ea7767845a78
Emashzed <Emashzed@users.noreply.github.com>
2024-06-03 23:25:03 +0200

    Audio is now linearly interpolated. Better audio quality

--- Assets/curif/LibRetroWrapper/cwrapper/audio.c
+++ Assets/curif/LibRetroWrapper/cwrapper/audio.c
@@ -3,14 +3,15 @@
 // #define AUDIO_FRAME_DEBUG
 // #define AUDIO_CANCEL
 
-static handlers_t *handlers;
+static handlers_t* handlers;
 
 // audio buffer ===============
 
 #define QUEST_AUDIO_FREQUENCY 48000
 #define MAX_AUDIO_BATCH_SIZE (1024 * 8)
 static float AudioBatch[MAX_AUDIO_BATCH_SIZE]; // Downsampled audio buffer
-static float inBuffer[MAX_AUDIO_BATCH_SIZE];   // internal transaction buffer
+static float inBufferL[MAX_AUDIO_BATCH_SIZE / 2];   // internal transaction buffer for Left channel
+static float inBufferR[MAX_AUDIO_BATCH_SIZE / 2];   // internal transaction buffer for Right channel
 static size_t AudioBatchOccupancy = 0;
 
 static AudioBufferLock AudioBufferLockCB;
@@ -18,83 +19,95 @@ static AudioBufferUnlock AudioBufferUnLockCB;
 
 void wrapper_audio_sample_cb(int16_t left, int16_t right) { return; }
 
-static size_t wrapper_audio_sample_batch_cb(const int16_t *data,
-                                            size_t frames) {
-  size_t consumedFrames = 0;
-  
+static size_t wrapper_audio_sample_batch_cb(const int16_t* data,
+	size_t frames) {
+	size_t consumedFrames = 0;
+
 #ifdef AUDIO_FRAME_DEBUG
-  wrapper_environment_log(
-      RETRO_LOG_INFO, "[wrapper_audio_sample_batch_cb] frames: %i\n", frames);
+	wrapper_environment_log(
+		RETRO_LOG_INFO, "[wrapper_audio_sample_batch_cb] frames: %i\n", frames);
 #endif
 
 
-  if (data == NULL || frames < 2) {
+	if (data == NULL || frames < 2) {
 #ifdef AUDIO_FRAME_DEBUG
-    wrapper_environment_log(RETRO_LOG_ERROR,
-                            "[wrapper_audio_sample_batch_cb] no data received\n");
+		wrapper_environment_log(RETRO_LOG_ERROR,
+			"[wrapper_audio_sample_batch_cb] no data received\n");
 #endif
-    return 0;
-  }
+		return 0;
+	}
 
-  if (frames % 2 != 0) {
-    // Handle the case where frames is not even (e.g., by skipping the last odd
-    // sample)
-    frames--;
-  }
-  size_t totalFrames = frames * 2;
+	if (frames % 2 != 0) {
+		// Handle the case where frames is not even (e.g., by skipping the last odd
+		// sample)
+		frames--;
+	}
+	size_t totalFrames = frames * 2;
 
 #ifdef AUDIO_CANCEL
-  return frames;
+	return frames;
 #endif
 
-  if (AudioBatchOccupancy >= MAX_AUDIO_BATCH_SIZE ||
-      totalFrames > MAX_AUDIO_BATCH_SIZE) {
+	if (AudioBatchOccupancy >= MAX_AUDIO_BATCH_SIZE ||
+		totalFrames > MAX_AUDIO_BATCH_SIZE) {
 #ifdef AUDIO_FRAME_DEBUG
-    wrapper_environment_log(RETRO_LOG_ERROR,
-                            "[wrapper_audio_sample_batch_cb] buffer exceeded - "
-                            "frames:%i AudioBatchOccupancy:%i\n",
-                            totalFrames, AudioBatchOccupancy);
+		wrapper_environment_log(RETRO_LOG_ERROR,
+			"[wrapper_audio_sample_batch_cb] buffer exceeded - "
+			"frames:%i AudioBatchOccupancy:%i\n",
+			totalFrames, AudioBatchOccupancy);
 #endif
-    return 0;
-  }
+		return 0;
+	}
 
-  for (size_t i = 0; i < totalFrames; i++) {
-    // Dividing by 32768.0 scales the data from
-    // the range of -32768 to 32767 to the range of -1.0 to 1.0
-    float value = (float)data[i] / 32768.0f;
-    inBuffer[i] = value;
-  }
+	// Copy data into the input buffers
+	for (size_t i = 0; i < frames; i++) {
+		inBufferL[i] = (float)data[2 * i] / 32768.0f;
+		inBufferR[i] = (float)data[2 * i + 1] / 32768.0f;
+	}
 
-  AudioBufferLockCB(); // Lock the AudioBatch for synchronization
+	AudioBufferLockCB(); // Lock the AudioBatch for synchronization
 
-  double ratio =
-      (double)wrapper_environment_get_sample_rate() / QUEST_AUDIO_FREQUENCY;
+	double ratio = (double)wrapper_environment_get_sample_rate() / QUEST_AUDIO_FREQUENCY;
+	size_t outSample = 0;
 
-  size_t outSample = 0;
-  while (1) {
-    int inBufferIndex = (int)(outSample++ * ratio);
-    if (inBufferIndex >= (int)totalFrames)
-      break;
+	while (1) {
+		double inBufferIndex = outSample * ratio;
+		int index = (int)inBufferIndex;
+		double frac = inBufferIndex - index; // fractional part
+		outSample++;
 
-    AudioBatch[AudioBatchOccupancy] = inBuffer[inBufferIndex];
-    consumedFrames++;
-    AudioBatchOccupancy++;
+		if (index + 1 >= frames) {
+			break;
+		}
 
-    if (AudioBatchOccupancy >= MAX_AUDIO_BATCH_SIZE)
-      break;
-  }
+		// Linear interpolation for left channel
+		float leftSample = (1 - frac) * inBufferL[index] + frac * inBufferL[index + 1];
+		// Linear interpolation for right channel
+		float rightSample = (1 - frac) * inBufferR[index] + frac * inBufferR[index + 1];
 
-  AudioBufferUnLockCB(); // Unlock the AudioBatch
+		// Store the interpolated samples in the combined output buffer
+		AudioBatch[AudioBatchOccupancy] = leftSample;
+		AudioBatchOccupancy++;
+		AudioBatch[AudioBatchOccupancy] = rightSample;
+		AudioBatchOccupancy++;
+		consumedFrames += 2; // increment by 2 for stereo
 
-  return consumedFrames;
+		if (AudioBatchOccupancy >= MAX_AUDIO_BATCH_SIZE) { // Check for buffer overflow
+			break;
+		}
+	}
+
+	AudioBufferUnLockCB(); // Unlock the AudioBatch
+
+	return consumedFrames;
 }
 
 // Function to get the buffer pointer
-float *wrapper_audio_get_audio_buffer_pointer() { return AudioBatch; }
+float* wrapper_audio_get_audio_buffer_pointer() { return AudioBatch; }
 
 // Function to get the buffer occupancy
 size_t wrapper_audio_get_audio_buffer_occupancy() {
-  return AudioBatchOccupancy;
+	return AudioBatchOccupancy;
 }
 
 // Function to consume the first part of the buffer and move the rest to
@@ -103,46 +116,46 @@ size_t wrapper_audio_get_audio_buffer_occupancy() {
 void wrapper_audio_consume_buffer(size_t consumeSizeFloat) {
 
 #ifdef AUDIO_FRAME_DEBUG
-  wrapper_environment_log(
-      RETRO_LOG_INFO,
-      "[wrapper_audio_consume_buffer] consumeSize:%i AudioBatchOccupancy:%i\n",
-      consumeSizeFloat, AudioBatchOccupancy);
+	wrapper_environment_log(
+		RETRO_LOG_INFO,
+		"[wrapper_audio_consume_buffer] consumeSize:%i AudioBatchOccupancy:%i\n",
+		consumeSizeFloat, AudioBatchOccupancy);
 #endif
 
-  size_t size;
-  size_t consume = consumeSizeFloat;
-  if (consume > AudioBatchOccupancy) {
-    wrapper_environment_log(
-        RETRO_LOG_ERROR,
-        "[wrapper_audio_consume_buffer] consume size exceded occupancy: %i\n",
-        consume);
-    consume = AudioBatchOccupancy;
-  }
+	size_t size;
+	size_t consume = consumeSizeFloat;
+	if (consume > AudioBatchOccupancy) {
+		wrapper_environment_log(
+			RETRO_LOG_ERROR,
+			"[wrapper_audio_consume_buffer] consume size exceded occupancy: %i\n",
+			consume);
+		consume = AudioBatchOccupancy;
+	}
 
-  // Use memmove to move the remaining data to the beginning of the buffer
-  size = (AudioBatchOccupancy - consume) * sizeof(float);
-  if (size)
-    memmove(AudioBatch, AudioBatch + consume, size);
+	// Use memmove to move the remaining data to the beginning of the buffer
+	size = (AudioBatchOccupancy - consume) * sizeof(float);
+	if (size)
+		memmove(AudioBatch, AudioBatch + consume, size);
 
-  AudioBatchOccupancy -= consume; // Update occupancy
+	AudioBatchOccupancy -= consume; // Update occupancy
 }
 
 void wrapper_audio_init(AudioBufferLock audioBufferLockCB,
-                        AudioBufferUnlock audioBufferUnLockCB) {
-  wrapper_environment_log(RETRO_LOG_INFO, "[wrapper_audio_init]\n");
-  handlers = wrapper_environment_get_handlers();
-  if (!handlers)
-    return;
+	AudioBufferUnlock audioBufferUnLockCB) {
+	wrapper_environment_log(RETRO_LOG_INFO, "[wrapper_audio_init]\n");
+	handlers = wrapper_environment_get_handlers();
+	if (!handlers)
+		return;
 
-  AudioBufferUnLockCB = audioBufferUnLockCB;
-  AudioBufferLockCB = audioBufferLockCB;
+	AudioBufferUnLockCB = audioBufferUnLockCB;
+	AudioBufferLockCB = audioBufferLockCB;
 
-  handlers->retro_set_audio_sample(&wrapper_audio_sample_cb);
-  handlers->retro_set_audio_sample_batch(&wrapper_audio_sample_batch_cb);
+	handlers->retro_set_audio_sample(&wrapper_audio_sample_cb);
+	handlers->retro_set_audio_sample_batch(&wrapper_audio_sample_batch_cb);
 }
 
 void wrapper_audio_free() {
-  AudioBufferLockCB(); // Lock the AudioBatch for synchronization
-  AudioBatchOccupancy = 0;
-  AudioBufferUnLockCB(); // Unlock the AudioBatch
+	AudioBufferLockCB(); // Lock the AudioBatch for synchronization
+	AudioBatchOccupancy = 0;
+	AudioBufferUnLockCB(); // Unlock the AudioBatch
 }
