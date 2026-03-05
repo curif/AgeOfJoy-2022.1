18314986398137fff23217c1de32443fff97c6e1
Fabio Curi <curif50@gmail.com>
2026-03-04 22:02:39 -0300

    fixed deadlock on cabinet replace.

--- Assets/curif/LibRetroWrapper/basic/functions/cabs.cs
+++ Assets/curif/LibRetroWrapper/basic/functions/cabs.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Round<TInput, TOutput>;
 
 class CommandFunctionCABROOMCOUNT : CommandFunctionNoExpressionBase
 {
@@ -75,10 +76,18 @@ class CommandFunctionCABROOMREPLACE : CommandFunctionExpressionListBase
         if (!config.GameRegistry.CabinetExists(cabinetDBName))
             return new BasicValue(0); //fail
 
-        bool result = config.CabinetsController.ReplaceInRoom((int)vals[0].GetNumber(),
-                                                                roomName,
-                                                                cabinetDBName).Result;
+        // We cannot use .Result here because ReplaceInRoom uses await on Unity API calls.
+        // Calling .Result on the main thread causes a deadlock.
+        // We launch it as a fire-and-forget task.
+        // _ = someAsyncMethod(); it is a signal to both the compiler and other developers that:
+        // 1.You are starting a Task: You are calling an async method that returns a Task.
+        // 2.You are not waiting: You are intentionally not using await.You want the code to continue running the very next line immediately without waiting forsomeAsyncMethod to finish.
+        //3.You are ignoring the result: You don't care about the return value or the Task object itself.
+        _ = config.CabinetsController.ReplaceInRoom((int)vals[0].GetNumber(),
+                                                    roomName,
+                                                    cabinetDBName);
 
-        return new BasicValue(result ? 1 : 0);
+        // We return 1 immediately assuming the process started correctly.
+        return new BasicValue(1);
     }
 }
