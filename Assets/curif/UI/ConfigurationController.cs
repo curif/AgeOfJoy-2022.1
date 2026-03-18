a7099a7fe926078fe52ad9911b7d868bc5d244fa
Fabio Curi <curif50@gmail.com>
2026-03-18 11:57:32 -0300

    onevents working con config cabinets

--- Assets/curif/UI/ConfigurationController.cs
+++ Assets/curif/UI/ConfigurationController.cs
@@ -2340,38 +2340,7 @@ public class ConfigurationController : MonoBehaviour
                 })
             .End()
 
-            .Sequence("AGEBasicRunning")
-              .Condition("On AGEBasic", () => status == StatusOptions.onRunAGEBasicRunning)
-              .Do("Process", () =>
-              {
-                  if (DateTime.Now > AGEBasicRunTimeout)
-                  {
-                      AGEBasic.Shutdown();
-                      status = StatusOptions.onRunAGEBasic;
-                      return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
-                  }
 
-                  if (AGEBasic.LastRuntimeException != null)
-                      ((GenericTimedLabel)AGEBasicContainer.GetWidget("RuntimeStatus")).Start(4);
-
-                  if (AGEBasic.IsRunning())
-                      return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Continue;
-
-                  if (AGEBasic.IsRunningInBackground())
-                  {
-                      // The program finished executing its initial pass but is now waiting for events in the background.
-                      // Stop the UI from blocking the screen, but let the user know it's active in the background.
-                      inputDictionary["action"] = false; // Prevent instant dismissal from holding the button
-                      AGEBasicWaitForPressAKey = true;
-                      AGEBasicShowLastRuntimeError(); // Will show "active in background" based on the updated logic
-                      status = StatusOptions.onRunAGEBasic; // MUST CHANGE STATUS to go to the UI interaction loop!
-                      return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success; // MUST RETURN SUCCESS to finish this sequence
-                  }
-
-                  status = StatusOptions.onRunAGEBasic;
-                  return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
-              })
-            .End()
             .Sequence("AGEBasic")
               .Condition("On AGEBasic", () => status == StatusOptions.onRunAGEBasic)
               .Do("Init", () =>
@@ -2403,6 +2372,12 @@ public class ConfigurationController : MonoBehaviour
                             status = StatusOptions.onMainMenu;
                             return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
                         }
+
+                        else if (w.name == "Compile")
+                        {
+                            AGEBasicCompile();
+                        }
+
                         else if (w.name == "run")
                         {
                             AGEBasicRun();
@@ -2410,10 +2385,7 @@ public class ConfigurationController : MonoBehaviour
                             status = StatusOptions.onRunAGEBasicRunning;
                             return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
                         }
-                        else if (w.name == "Compile")
-                        {
-                            AGEBasicCompile();
-                        }
+
                         else if (w.name == "CompError" || w.name == "RunTimeError")
                         {
                             AGEBasicWaitForPressAKey = true;
@@ -2431,6 +2403,36 @@ public class ConfigurationController : MonoBehaviour
                     scr.DrawScreen();
                     return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Continue;
                 })
+            .End()
+
+            .Sequence("AGEBasicRunning")
+              .Condition("On AGEBasic", () => status == StatusOptions.onRunAGEBasicRunning)
+              .Do("Process", () =>
+              {
+                  if (DateTime.Now > AGEBasicRunTimeout)
+                  {
+                      AGEBasic.Shutdown();
+                      status = StatusOptions.onRunAGEBasic;
+                      return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
+                  }
+
+                  if (AGEBasic.LastRuntimeException != null)
+                      ((GenericTimedLabel)AGEBasicContainer.GetWidget("RuntimeStatus")).Start(4);
+
+                  if (AGEBasic.IsRunning() || AGEBasic.IsRunningInBackground())
+                      return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Continue;
+
+                  // The program finished executing its initial pass but is now waiting for events in the background.
+                  // Stop the UI from blocking the screen, but let the user know it's active in the background.
+                  //inputDictionary["action"] = false; // Prevent instant dismissal from holding the button
+                  //AGEBasicWaitForPressAKey = true;
+                  AGEBasicShowLastRuntimeError(); // Will show "active in background" based on the updated logic
+                  status = StatusOptions.onRunAGEBasic; // MUST CHANGE STATUS to go to the UI interaction loop!
+                  return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success; // MUST RETURN SUCCESS to finish this sequence
+
+                  //status = StatusOptions.onRunAGEBasic;
+                  //return CleverCrow.Fluid.BTs.Tasks.TaskStatus.Success;
+              })
             .End()
 
             .Sequence("EXIT")
