d4829308a674f84a3da2cbd8094866762a43c278
Fabio Curi <curif50@gmail.com>
2026-07-28 18:36:06 -0300

    fix backrooms.

--- Assets/curif/LibRetroWrapper/BackgroundSoundController.cs
+++ Assets/curif/LibRetroWrapper/BackgroundSoundController.cs
@@ -24,7 +24,7 @@ public class BackgroundSoundController : MonoBehaviour
         roomConfiguration = RoomConfigurationGameObject.GetComponent<RoomConfiguration>();
         OnEnable();
         change();
-    }
+    }w
     void changeWithBackgroundData(ConfigInformation.Background background)
     {
         GameObject[] backgroundSoundObjects = GameObject.FindGameObjectsWithTag("backgroundsound");
